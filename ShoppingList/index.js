74d845ce71be6dc59b0a7d7026d8ec6c3ce134e7
Fabian Lema <fabianlemac@gmail.com>
2019-07-11 14:51:04 -0500

    Added add and check function

---
+++ ShoppingList/index.js
@@ -0,0 +1,38 @@
+function getHttp(object){
+	return '<li>' +
+		'<span class="shopping-item">' + object + '</span>' +
+	        '<div class="shopping-item-controls">' +
+	          '<button class="shopping-item-toggle">' +
+	            '<span class="button-label">check</span>' +
+	          '</button>' +
+	          '<button class="shopping-item-delete">' +
+	            '<span class="button-label">delete</span>' +
+	          '</button>' +
+	        '</div>' +
+		'</li>';
+}
+
+function createCalls(){
+	$('#js-shopping-list-form').on('submit', e => {
+		e.preventDefault();
+		$('.shopping-list').append(getHttp($('#shopping-list-entry').val()));
+		$('#shopping-list-entry').val('');
+	});
+	$('#js-shopping-list-form').on('keyup', e => {
+		if(e.keyCode == 13){
+			$('.shopping-list').append(getHttp($('#shopping-list-entry').val()));
+			$('#shopping-list-entry').val('');
+		}
+	});
+
+	$('li .shopping-item-controls .shopping-item-toggle').on('click', e => {
+		$(e.target).parent().parent().parent().find('.shopping-item').addClass('shopping-item__checked');
+		//e.preventDefault();
+		//$(e.target).parent().addClass('shopping-item__checked');
+		console.log($(e.target).parent().parent().parent().find('.shopping-item'));
+		//e.target
+		//$('.shopping-list').append(getHttp($('#shopping-list-entry').val()));
+	});
+}
+
+$(createCalls);
